6878fe99cd7886ebc29629ac9e39b08f81f7054c
Ché Armstrong <neuroscience.che@gmail.com>
2019-11-23 20:39:02 +0000

    Fix and improve hints (#6)

--- code/getPopular.js
+++ code/getPopular.js
@@ -8,7 +8,7 @@ const getTmdbId = require('./lib/getTmdbId.js')
 module.exports.function = function getPopular(provider, $vivContext) {
   try {
     provider = provider ? provider.toString() : provider // To deal with bug
-    const { locale } = $vivContext
+    const { locale, bixbyUserId } = $vivContext
     
     const content = getPopularTitlesByProvider(provider, locale)
 
